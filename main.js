2239d942a63d06dcd0cf05f819c45362fce9fbe6
s-shpandey <s-shpandey@lwsd.org>
2023-12-02 22:08:15 -0800

    merged apis, run on local server

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const authUtils = require('./utils/authUtils.js');
+const calculations = require('./calculations/calculations.js');
 
 // Create an Express application
 const app = express();
@@ -20,9 +21,9 @@ app.use('/getToken', authUtils);
 
 // Define a route to serve the index.html file
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html');
+    //res.sendFile(__dirname + '/views/index.html');
 
-    const authToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6IjdudWRic2U1ODEiLCJ0b2tlbiI6eyJpdiI6ImFiZjE0MTdjM2NmZTMyY2YwNTQwZjcxZTlmYTg3MDc0IiwiY29udGVudCI6IjllZDg5N2ExOTcxNmJkZmQ0MGVhNjAwNThiYTNjMGNhZTZhNmI1NTZhZGQ5MWFlOWI1YTczM2FiNGM0MDYwOWJiNDYzNGQ0MTJhMjViMmM5NWQwZmJhZTE5MDk1MjgzMGVjOTI5MmM1MjA1OWRmM2JhOTY4MDg4OWVkMDA2M2FhMDE0Zjk4NWQwMzY0NDkzZDZkY2NkYzhkNWI1NDViZmY3N2Y4Nzg3YTNiN2ViNDM5YTY3MWI2ZjA5NTFjYjg1Yzk2MjljNTg3MDA5OTVmZTZiMGM0NzRmMzc4OTI3NDM2MGYzZDNjNjRlMTkxMjZlMzFiODQ4MDM3OTFmYTdhMTFkZDg0Y2E4YjBjZTE5M2JiYmJmMTU4MWVkNDZmYTlmZGVhOWUwNzljNWU2YjFlYTAzYjZjMjM3NjU5MWU5Y2U0ZWI1Yzc5ZGJiNWQyNzIyNThlZTBkMzJiNDBjOGJkM2UzZjA0ODgzODY2ZTFmZmI1Y2IxYTZhZThjZDM1NGVlOWJhYzAzOTE0ODM4NDg3YmU3NDAwZjA2NzdkMTFhYzQxNDZjODEzNzc0NWY3Mjk3NjVkOGYwMjVmMGJlNTAwNmQxOTBhMzc5NWNiOWIwNjU1YWU1YTJmMzYzZTViNTA2M2IxOTUyODA4NDUxYTBhZWQ3YTdhOTNjNGRjNjJkMzViM2MyMDgzNmUwYmQ0M2UwYzQ4OWI2ZmJmZDQxMTNjZDU0ZGYxNTQzYmI2MjlmNzM0Y2JkMWQ3NmZiOThiOTQ2MjdhYzMzZDI5N2E5ZGNlZjgyNzY2YTVlMzdjMTRlN2VjM2E4NWZlODA4MTZiNGE3NDNmYTUxMzRkMWExMjZhZjA5ZGFlOWE0MmE2Y2UzYzhjZDdmYzhmIn0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiJhYmYxNDE3YzNjZmUzMmNmMDU0MGY3MWU5ZmE4NzA3NCIsImNvbnRlbnQiOiI5MGQzYTU5MTgyMzk5NGY1NzhjZjRjMDdiMWUxZDk4ODlhODViNzZhYmM4NjcwOTU5MzkxNGJlNDc3NDgyNWY3ODk0Yzc1MmM2YzM3YjllMjAyMjFiM2RmIn0sImp0aSI6ImQ4NzQ4MjRlLTUxZjUtNDE2NC05M2QyLWM3MDFhNDczOGQxNyIsImlhdCI6MTcwMTU3MjM1MCwiZXhwIjoxNzAxNTc1OTQ5fQ.YwQhcJOP3IYQ3LUCEs4EGZFTnIFOGke8IMzvw5YtHPc';
+    const authToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBJZCI6IjdudWRic2U1ODEiLCJ0b2tlbiI6eyJpdiI6IjY5ZGIzMjY3ZTg1OTk4MWIwNjk5ODFlYjBhMDdjZGVkIiwiY29udGVudCI6ImJhYTRlNDRmYTIxMzdjOTNhNDUyZWZhYzEwNGRkZWI2ZmVmODZjZTNkMzdjZGFmNDkzODQ4YWFhYWJkZGUyMDk2M2JhY2FiY2JiODNiMjA4NGIyZjkwMDcwYTdlNTRjZGQ5MDExODZkYWJiNzczZGY5YmJmZTk4NTdmNTdmZWI3MzYxMzE4YzRkYWIzNzg2NmM2MGY4Zjc2ZmRhMjljMzE1NzgyNTZhYWQ3ZTFlYzRkYzc5MzBkODA4ODQxNmU5MTkxYjg2NzNmMjQzMzA0ZjlmNGNmZGFlOTc4N2YzYzRkOTNhMDMyYjY5NzI2NGJhMWMwNDBjNWMzNzhkMDJiZWQzZGZlMmQzNzVkNjU5NmJjY2RmMGI1MWFjN2EzM2UzYWRhMmEzMTZjNDkyMDFkMDI3ODIwNjExMTNhZmRlZThiMTUxNmZlNjc3YTI2MWRjOTUzYmU0MjQ3MDQyZGU1YTYzZmI4OWY0ZTE4ZjYzMGMyMTg2ODY3NDgxMTgzYmZiZDA0ZGRhZTNiZDViZWVhMzc1MmJkOTI2YTU0OTNiNGQxMGY3Y2YwNTFjYWQ1YTJjZjk3NTdiODg0MDI1YjUzOWE0NThiNTE5YTJlNmNjZWE1NTJiZTdmYjQyNTQ0ZjU0ZGE4MWQxMTVmNjBjNmJmYWU0NmU1NmIyNDU2M2VkN2Q0OTBkOGI5MWQwOWZkZmI0ZTJhOTJjYjMxYTRiMmQ2OTU3OGJmZjk5NzBmYTg4M2FkZDhjMTM3ZTQyMDU1MTU4NWNjMDY0MDFmMDEzODUzMWFiYzg3NzRiOGMwZGY3MTEyMjYxZjg3MzZlZTY4YTYyYzJkZWFkOWMyMmY3ZDhlNzBhZWQwYTllZTNmYTQxNTAyYWM2OTAxIn0sInNlY3VyaXR5VG9rZW4iOnsiaXYiOiI2OWRiMzI2N2U4NTk5ODFiMDY5OTgxZWIwYTA3Y2RlZCIsImNvbnRlbnQiOiI4N2FmY2M3ZGI5M2E3YmI4YWUwM2Q1OWQ2ODQzZmZlMWM1YTc0MmQ3Y2QyYWJiYjdiZmVhOTdmOGM4ZWVhNTYxNjM5YmNlYjBmNmE4YWE2ZjQyMWM4OTM5In0sImp0aSI6IjIxZGVkMDc5LWQ3MGItNDBkNy04MGRhLTllOGExOTI2NzRmNyIsImlhdCI6MTcwMTU4MjEwMywiZXhwIjoxNzAxNTg1NzAzfQ.B39WP0-mXDrbXTdXwuaLMht9FiwhYZ7Nr2A8Jzt_L9I';
     const baseUrl = 'https://api.iq.inrix.com/findRoute';
 
     const departureCoordinates = '37.75961056112695,-122.45156188050721';
@@ -44,22 +45,24 @@ app.get('/', (req, res) => {
     };
 
     fetch(apiUrl, requestOptions)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`HTTP error! Status: ${response.status}`);
-            }
-            return response.json(); // Assuming the API returns JSON data
-        })
+        .then(response => response.json())
         .then(data => {
-            // Handle the API response data here
-            console.log(data);
-         
+            // Store the fetched data in a variable
+            res.send(data);
+        }).catch(err=>{
+            console.error('Error Message: ', err);
         })
-        .catch(error => {
-            console.error('Error fetching API data:', error);
-            
-        });
 });  
+
+app.get("/waittime", async (req, res) => {
+    var myHeaders = new Headers();
+    myHeaders.append("4fcg10Cphlq1jmEHRAbsBxTFrWYm7ITK", "g9VXBhKbGCy66C0Y");
+    const response = await fetch("https://api.flysfo.com/sfo/v1.0/checkpoints/e890e3d4-7a42-11e7-bb31-be2e44b06b34/waittime");
+    let output = await response.json()
+    console.log(output)
+    res.status(200).json(output);
+
+});
 
 // Start the server and listen on the specified port
 app.listen(port, () => {
